01da6be9bcdc52423e576c736581e6395eef293b
Victor Mielke <vicemanu31@outlook.com>
2023-07-03 22:18:40 -0300

    pagina de login criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import RoutesApp from './Routes'
 
 export default function App() {
 
   return (
     <>
+      <RoutesApp/>
     </>
   )
 }
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,39 @@
+import './signin.css'
+import logo from '../../assets/logo.png'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
 export default function SignIn() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return(
-        <>
-        pagina login
-        </>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt="logo do sistema de chamados" />
+                </div>
+                <form action="">
+                    <h1>Login</h1>
+                    <input 
+                    type="text" 
+                    placeholder='email@email.com' 
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    />
+                    <input 
+                    type="password" 
+                    placeholder='**********' 
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    />
+
+                    <button type="submit">Acessar</button>
+                </form>
+
+                <Link to="/register">Criar uma conta</Link>
+            </div>
+        </div>
     )
 }
