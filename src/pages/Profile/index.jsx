20eaa927c1f624d5fd7781666c960b7610d760c8
Victor Mielke <vicemanu31@outlook.com>
2023-07-06 22:14:46 -0300

    preparando para usar o codigo de recebimento do firebase

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -10,10 +10,28 @@ import './profile.css'
 export default function Profile() {
 
 
-    const { user } = useContext(AuthContext);
+    const { user, storageUser, setUser, logout } = useContext(AuthContext);
 
     const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
+    const [imageAvatar, setImageAvatar] = useState(null)
 
+    const [nome, setNome] = useState(user && user.name)
+    const [email, setEmail] = useState(user && user.email)
+
+    function handleFile(e) {
+        if(e.target.files[0]) {
+            const image = e.target.files[0];
+
+
+            if(image.type === 'image/jpeg' || image.type === 'image/png') {
+                setImageAvatar(image)
+                setAvatarUrl(URL.createObjectURL(image))
+            } else {
+                alert("envie uma imagem do tipo PNG ou JPEG")
+                setImageAvatar(null)
+            }
+        }
+    }
 
     return(
         <div>
@@ -33,7 +51,7 @@ export default function Profile() {
                                 <FiUpload color="#FFF" size={25}/>
                             </span>
 
-                            <input type="file" accept="image/*" /><br/>
+                            <input type="file" accept="image/*" onChange={handleFile} /><br/>
                             {avatarUrl === null ? (
                                 <img src={avatar} alt="Foto de perfil" width={250} height={250}/>
                             ): (
@@ -42,10 +60,10 @@ export default function Profile() {
                         </label>
 
                         <label htmlFor="Nome">Nome</label>
-                        <input type="text" id="Nome" placeholder="Seu nome" />
+                        <input type="text" value={nome} onChange={e => setNome(e.target.value)} id="Nome" placeholder="Seu nome" />
 
                         <label htmlFor="">Email</label>
-                        <input type="text" id="" placeholder="Teste@tesete.com" disabled={true} />
+                        <input type="text" id="" value={email} disabled={true} />
 
                         <button type="submit">Salvar</button>
 
@@ -53,7 +71,7 @@ export default function Profile() {
                 </div>
 
                 <div className="container">
-                    <button className="logout-btn">Sair</button>
+                    <button className="logout-btn" onClick={e => logout()}>Sair</button>
                 </div>
             </div>
         </div>
